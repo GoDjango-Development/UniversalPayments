2965383130e07dbc80dbef5e5cf1ce0fde02e579
daniel2751241 <58035766+daniel2751241@users.noreply.github.com>
2023-08-23 05:19:15 -0400

    Add minor fixes on documentation

--- back_office/admin/square_documentation.py
+++ back_office/admin/square_documentation.py
@@ -11,6 +11,16 @@ class SquareDocumentationAdmin(admin.ModelAdmin):
     list_per_page = 20
     form = SquareDocumentationForm
     
+    def add_view(self, request, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['model_name'] = 'squaresdocumentation'
+        return super().add_view(request, form_url, extra_context=extra_context)
+    
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['model_name'] = 'squaresdocumentation'
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
+    
     def changelist_view(self, request, extra_context=None):
         extra_context = extra_context or {}
         first_obj = self.model.objects.first()
@@ -18,7 +28,7 @@ class SquareDocumentationAdmin(admin.ModelAdmin):
             if request.user.is_superuser:
                 return redirect(reverse('admin:documentation_squaresdocumentation_change', args=(first_obj.pk,)))
             else:
-                return redirect(reverse('admin:documentation_squaresdocumentation_change', args=(first_obj.pk,)))
+                return redirect(reverse_lazy('object_details', kwargs={'model_name': 'squaresdocumentation', 'pk': first_obj.pk}))
         return super().changelist_view(request, extra_context)
     
     def has_add_permission(self, request):
--- back_office/admin/wells_fargo_documentation.py
+++ back_office/admin/wells_fargo_documentation.py
@@ -11,12 +11,22 @@ class WellsFargoDocumentationAdmin(admin.ModelAdmin):
     list_per_page = 20
     form = WellsFargoDocumentationForm
     
+    def add_view(self, request, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['model_name'] = 'wellsfargodocumentation'
+        return super().add_view(request, form_url, extra_context=extra_context)
+    
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['model_name'] = 'wellsfargodocumentation'
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
+    
     def changelist_view(self, request, extra_context=None):
         extra_context = extra_context or {}
         first_obj = self.model.objects.first()
         if first_obj is not None:
             if request.user.is_superuser:
-                return redirect(reverse('admin:applications_wellsfargodocumentation_change', args=(first_obj.pk,)))
+                return redirect(reverse('admin:documentation_wellsfargodocumentation_change', args=(first_obj.pk,)))
             else:
                 return redirect(reverse_lazy('object_details', kwargs={'model_name': 'wellsfargodocumentation', 'pk': first_obj.pk}))
         return super().changelist_view(request, extra_context)
