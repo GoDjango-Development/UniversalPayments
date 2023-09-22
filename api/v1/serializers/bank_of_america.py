140b261b1a9804eec422dc2808593dae448ae303
daniel2751241 <58035766+daniel2751241@users.noreply.github.com>
2023-09-22 05:46:31 -0400

    Mark fields as required

--- api/v1/serializers/bank_of_america.py
+++ api/v1/serializers/bank_of_america.py
@@ -4,22 +4,22 @@ from django.core.validators import RegexValidator, MaxValueValidator, MinValueVa
 from payments.models import BankOfAmericaPayment
 
 class CreditCardSerializer(serializers.Serializer):
-    card_number = serializers.CharField(max_length=16, validators=[RegexValidator(r'^\d+$')])
-    expiration_month = serializers.IntegerField(MinValueValidator(1), MaxValueValidator(12))
-    expiration_year = serializers.IntegerField(MinValueValidator(datetime.today().year))
+    card_number = serializers.CharField(max_length=16, validators=[RegexValidator(r'^\d+$')], required=True)
+    expiration_month = serializers.IntegerField(MinValueValidator(1), MaxValueValidator(12), required=True)
+    expiration_year = serializers.IntegerField(MinValueValidator(datetime.today().year), required=True)
     
 class CustomerAddresSerializer(serializers.Serializer):
-    first_name = serializers.CharField(max_length=50)
-    last_name = serializers.CharField(max_length=50)
-    address = serializers.CharField(max_length=50)
-    locality = serializers.CharField(max_length=50)
-    area = serializers.CharField(max_length=50)
-    postal = serializers.IntegerField(MaxLengthValidator(5))
-    country = serializers.CharField(max_length=40)
-    email = serializers.CharField(max_length=100)
-    phone = serializers.CharField(max_length=20)
+    first_name = serializers.CharField(max_length=50, required=True)
+    last_name = serializers.CharField(max_length=50, required=True)
+    address = serializers.CharField(max_length=50, required=True)
+    locality = serializers.CharField(max_length=50, required=True)
+    area = serializers.CharField(max_length=50, required=True)
+    postal = serializers.IntegerField(MaxLengthValidator(5), required=True)
+    country = serializers.CharField(max_length=40, required=True)
+    email = serializers.CharField(max_length=100, required=True)
+    phone = serializers.CharField(max_length=20, required=True)
 
 class PaymentDataSerializer(serializers.Serializer):
     total = serializers.DecimalField(max_digits=15, decimal_places=15)
-    credit_card = CreditCardSerializer()
+    credit_card = CreditCardSerializer(required=True)
     customer_address = CustomerAddresSerializer(required=True)
