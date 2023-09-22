a4a2b745048f14f14c6ac5f104fc81f08080e129
daniel2751241 <58035766+daniel2751241@users.noreply.github.com>
2023-09-22 05:04:05 -0400

    Add boa api urls

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from api.v1.views.bank_of_america_payments import AllBoAPayments, BoAPaymentByTransactionUUID, CreateBoAPayment
 from api.v1.views.square_payments import AllSquarePayments, CreateSquarePayment, SquarePaymentByTransactionUUID
 from api.v1.views.wells_fargo_payment import AllWFPayments, CreateWFPayment, WFPaymentByTransactionUUID 
 from api.v1.views.security import LoginView
@@ -11,4 +12,7 @@ urlpatterns = [
     path('create_square_payments/',CreateSquarePayment.as_view()),
     path('all_square_payments/',AllSquarePayments.as_view()),
     path('square_payments/<str:idempotency_key>', SquarePaymentByTransactionUUID.as_view()),
+    path('create_boa_payments/', CreateBoAPayment.as_view()),
+    path('all_boa_payments/', AllBoAPayments.as_view()),
+    path('boa_payments/<str:transaction_uuid>', BoAPaymentByTransactionUUID.as_view()),
 ]
